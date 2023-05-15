d2cde02cf7fcc99fffae27601bb1d81638f282c2
Yordy <yordyogandolopez@gmail.com>
2023-05-14 20:26:17 -0400

    guardado 1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+btn_bscr.onlclick = function(){
+    let buscr_anime = document.getElementById("buscr_anime").value;
+    if (buscr_anime == 'oshi no ko') {
+        panel.innerHTML = "https://www.youtube.com/watch?v=unGveIGwlqc"
+    }
+
+}
